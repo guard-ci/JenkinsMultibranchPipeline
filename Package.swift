2cebbd4bf746b60ca27fa122a101a66529278dd7
guard <nghialvbot@gmail.com>
2016-06-09 22:42:16 +0900

    symlink swift internal module

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftDemo",
     targets: [
-        Target(name: "Basic", dependencies: [])
+        Target(name: "Basic", dependencies: []),
+        Target(name: "Foo", dependencies: [.Target(name: "Basic")]),
+        Target(name: "Bar", dependencies: [.Target(name: "Basic")])
     ]
 )
