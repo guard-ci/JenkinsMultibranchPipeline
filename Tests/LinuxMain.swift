980842baeecdf6dae40b94255fc2f6dccb07a274
guard <nghialvbot@gmail.com>
2016-06-09 23:15:23 +0900

    add swift code for Foo module

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,12 @@
 import XCTest
 import BasicTestSuite
 import BarTestSuite
+import FooTestSuite
 
 var tests = [XCTestCaseEntry]()
 
 tests += BasicTestSuite.allTests()
 tests += BarTestSuite.allTests()
+tests += FooTestSuite.allTests()
 
 XCTMain(tests)
